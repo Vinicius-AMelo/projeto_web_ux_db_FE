0badcc6aee82e723f3cb306669eb0adaa9485f92
Vinícius Melo <vinicius.apol.melo@gmail.com>
2023-11-14 12:34:36 -0300

    :books: rota interna railway

--- server.js
+++ server.js
@@ -15,14 +15,19 @@ app.get('/database', (req, res) => {
     res.send([{name: 'Vinicius', age: 22}, {name: 'João', age: 23}])
 })
 
+app.get('/backend', (req, res)=> {
+    try {
+        const response = axios.get(`http://projeto_web_ux_db_be.railway.internal:${backendPort}/dinners`)
+        console.log(response.data)
+        res.send(response.data)
+    } catch (error) {
+        console.log(error)
+        res.send(error)
+    }
+})
+
 app.listen(port, () => {
     console.log(`Servidor express rodando na porta ${port}`);
     console.log(`http://localhost:${port}`)
 })
 
-try {
-    const response = axios.get(`http://projeto_web_ux_db_be.railway.internal:${backendPort}/dinners`)
-    console.log(response.data)
-} catch (error) {
-    console.log(error)
-}
